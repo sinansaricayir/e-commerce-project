4db0b41331ef1f1f68ec340b6de8704401b87cd0
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-03-15 19:24:21 +0300

    Sidebar Click Outside

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//! home sidebar start
+const btnOpenSidebar = document.querySelector("#btn-menu")
+const sidebar = document.querySelector("#sidebar")
+const btnCloseSidebar = document.querySelector("#close-sidebar")
+
+
+btnOpenSidebar.addEventListener("click", function () {
+    sidebar.style.left = "0"
+
+})
+
+btnCloseSidebar.addEventListener("click", function () {
+    sidebar.style.left = "-100%"
+})
+
+
+/* click outside start */
+document.addEventListener("click", (event) => {
+    if (!event.composedPath().includes(sidebar) && !event.composedPath().includes(btnOpenSidebar)) {
+        sidebar.style.left = "-100%"
+    }
+})
+/* click outside end */
+
+
+//! home sidebar end

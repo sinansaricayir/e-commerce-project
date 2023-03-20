cf80888ee5957d708fe03a8b1739ca60bd7c5bb3
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-03-21 00:44:58 +0300

    product item - glide js

--- js/glide.js
+++ js/glide.js
@@ -1,5 +1,23 @@
-new Glide('.glide', {
+const config = {
     type: 'carousel',
-    startAt: 0,
-    perView: 3
-}).mount()
+    perView: 4,
+    gap: 20,
+    autoplay: 3000,
+    breakpoints: {
+        992: {
+            perView: 3
+        },
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 1
+        },
+    }
+}
+
+new Glide('.product-carousel', config).mount()
+new Glide('.product-carousel2', config).mount()
+
+
+

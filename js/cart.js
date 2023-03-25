ab8dae15c8ee0e860c90d05d00a709f0e1182b1a
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-03-25 18:44:44 +0300

    remove cart item js

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,17 @@
-import { cart } from "./product.js"
+let cart = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart")) : []
+
 
 function displayCartProduct() {
     let results = ""
     const cartProduct = document.getElementById("cart-product")
-    cart.forEach((item, i) => {
-        console.log(item);
+    cart.forEach((item) => {
         results += `
         <tr class="cart-item">
             <td></td>
             <td class="cart-image">
                 <img src="${item.img.singleImage}" alt="">
-                <i class="bi bi-x delete-cart"></i>
+                <i class="bi bi-x delete-cart" data-id=${item.id}></i>
             </td>
             <td>${item.name}</td>
             <td>$${item.price.newPrice}</td>
@@ -20,7 +21,31 @@ function displayCartProduct() {
         `
     })
     cartProduct.innerHTML = results
+    removeCartItem()
 }
 
 
 displayCartProduct()
+
+
+function removeCartItem() {
+
+    const btnDeleteCart = document.querySelectorAll(".delete-cart");
+    let cartItem = document.querySelector(".header-cart-count")
+
+    btnDeleteCart.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            const id = e.target.dataset.id;
+            cart = cart.filter((item) => item.id !== Number(id));
+            displayCartProduct()
+            localStorage.setItem("cart", JSON.stringify(cart))
+            cartItem.innerHTML = cart.length
+        });
+    });
+}
+
+
+
+
+
+

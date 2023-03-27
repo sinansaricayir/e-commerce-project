376958e7a31c84d6283e8bf517df29c58a758570
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-03-28 00:05:25 +0300

    single product thumbnails js

--- js/single-product.js
+++ js/single-product.js
@@ -1,6 +1,6 @@
 import { product3 } from "./glide.js"
+import { thumbsActiveFunc } from "./single-product/thumbsActive.js"
 
-product3()
 
 const productId = localStorage.getItem("productId")
     ? JSON.parse(localStorage.getItem("productId"))
@@ -28,6 +28,23 @@ const productNewPrice = document.querySelector(".new-price")
 productNewPrice.innerHTML = `$${findProduct.price.newPrice.toFixed(2)}`
 
 /* product gallery */
-
 const singleImage = document.getElementById("single-image")
 singleImage.src = findProduct.img.singleImage
+
+/* gallery thumbs */
+
+const galleryThumbs = document.querySelector(".gallery-thumbs")
+let result = ""
+
+findProduct.img.thumbs.forEach((item) => {
+    result += `
+        <li class="glide__slide">
+            <img src="${item}" class="img-fluid" alt="">
+        </li>
+    `
+})
+
+galleryThumbs.innerHTML = result
+
+thumbsActiveFunc()
+product3()
---
+++ js/single-product/thumbsActive.js
@@ -0,0 +1,15 @@
+export function thumbsActiveFunc() {
+    const thumbs = document.querySelectorAll(".gallery-thumbs .img-fluid")
+    const singlImage = document.querySelector("#single-image")
+
+    thumbs.forEach((item) => {
+        item.addEventListener("click", () => {
+            thumbs.forEach((image) => {
+                image.classList.remove("active")
+            })
+
+            singlImage.src = item.src
+            item.classList.add("active")
+        })
+    })
+}
